90a3c082afba0a3cb3350d8436e061a9a1ba5ce4
VinoMurugan <vinothini.jit@gmail.com>
2023-08-14 13:02:38 -0400

    update

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+//Dom Element Selection
 const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".highscore");
 // const gridSize = 30;
 
+//Audio Setup
 const eatSound = new Audio('sound/Metal Gear Menu.mp3');
 eatSound.volume=0.4;
 
 const hitSound = new Audio('sound/Pan Hit Sound In Game - Pubg Game.mp3');
 hitSound.volume = 1.0;
 
+//Snake Body Color
 const snakeColors = ['blue', 'green', 'red', 'orange', 'purple'];
 
+//Game Variables
 let gameover = false;
 let foodX , foodY ;
 let snakeX = 5 , snakeY = 10;
@@ -20,7 +24,7 @@ let velocityY = 0;
 let setIntervalId;
 let score = 0;
 
-//function for sound
+//Sound Function
 function playEatSound() {
     eatSound.currentTime = 0;
     eatSound.play();
@@ -31,26 +35,28 @@ function playEatSound() {
     hitSound.play();
   }
   
-
-
 //getting high score from the local storage 
 let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
 
+//Changing Food Position
 const changeFoodPosition = () =>{
-    //passing a random value 0-30 as food position
+
+//passing a random value 0-30 as food position
 foodX = Math.floor(Math.random() * 30) + 1;
 foodY = Math.floor(Math.random() * 30) + 1;
+
 };
 
+//Game Over Handling
 const handleGameOver = () => {
-    //clearing the timer and reloading the page on game over
-    clearInterval(setIntervalId);
-   alert("GAME OVER ! press ok to replay....") ;
-   location.reload();
+ //clearing the timer and reloading the page on game over
+clearInterval(setIntervalId);
+alert("GAME OVER ! press ok to replay....") ;
+location.reload();
 }
 
-
+//Changing Direction
 const changeDirection = (e) =>{
     //change velocity value based on key
     if(e.key === "ArrowUp" && velocityY != 1) {
@@ -69,7 +75,7 @@ const changeDirection = (e) =>{
     initalGame();
 };
 
-
+//Initializing Game
 const initalGame = () => {
     if(gameover)return handleGameOver();
     let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
@@ -79,8 +85,6 @@ const initalGame = () => {
         snakeBody.push([foodX,foodY]); //pusing food position to snake body array
         // console.log(snakeBody);
         score++;  //increment score by 1
-       
-
         highScore = score >= highScore ? score : highScore;
         localStorage.setItem("high-score", highScore);
         scoreElement.innerText = `Score: ${score}`;
@@ -102,12 +106,12 @@ const initalGame = () => {
     snakeY +=velocityY;
     
     //checking if the snake head is out of wall
-   if (snakeX <= 0 || snakeX > 30 || snakeY <=0 || snakeY > 30) {
+    if (snakeX <= 0 || snakeX > 30 || snakeY <=0 || snakeY > 30) {
     gameover = true;
     playHitSound();
    }
 
-   for (let i = 0; i < snakeBody.length; i++) {
+    for (let i = 0; i < snakeBody.length; i++) {
     const segmentColor = snakeColors[i % snakeColors.length];
     htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}; background-color: ${segmentColor};"></div>`;
 
@@ -125,15 +129,14 @@ changeFoodPosition();
 document.addEventListener("keydown",changeDirection);
 
 
-
 // Select the start button element
 const startBtn = document.getElementById("startBtn");
 
+//Starting the Game 
 // Add event listener for the start button
 startBtn.addEventListener("click", startGame);
+
 // Function to start the game
 function startGame() {
-   
-
-    setIntervalId = setInterval(initalGame, 200);
+   setIntervalId = setInterval(initalGame, 200);
 }
